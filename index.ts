61efec2a275e788e4bf869bc070189e42e261600
Snehasagwal26 <167130203+Snehasagwal26@users.noreply.github.com>
2025-01-28 12:30:02 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,5 @@
-import { View, Text, FlatList } from "react-native";
-import fruits from "../components/List";
+mkdir app
+cd app
+touch index.tsx
 
-export default function Index() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Welcome to the App!</Text>
-      <FlatList
-        data={fruits}
-        renderItem={({ item }) => <Text>{item}</Text>}
-        keyExtractor={(item) => item}
-      />
-    </View>
-  );
-}
 
